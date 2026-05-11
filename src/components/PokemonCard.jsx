95d2b2d73c426e33e2a13f6a477e288746d28494
Fred <huertaserick@yahoo.ie>
2026-05-11 22:03:16 +0100

    Pokemon API, fetching, axios, Promise.all back learning

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+import "../styles/PokemonCard.css";
+
+function PokemonCard({ name, image }) {
+  return (
+    <div className="card">
+      <h3>{name}</h3>
+      <img src={image} alt={name} />
+    </div>
+  );
+}
+
+export default PokemonCard;
